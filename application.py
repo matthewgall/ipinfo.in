989f70c1ee3ecf6ca4a6d9f96426b1953f3fdba6
Matthew Gall <mgall@cloudflare.com>
2015-07-08 21:50:06 +0100

    Added more flexible version check based on application.py location

--- application.py
+++ application.py
@@ -40,7 +40,8 @@ def error404(error):
 
 @route('/version')
 def getVersion():
-    f = open(os.getenv('VERSION_PATH', '.git/refs/heads/master'), 'r')
+    dirname, filename = os.path.split(os.path.abspath(__file__))
+    f = open(os.getenv('VERSION_PATH', dirname + '/.git/refs/heads/master'), 'r')
     content = f.read()
     response.content_type = 'text/plain'
     return content
